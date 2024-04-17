39e2ffa2827b9dd82e076578c81780c49aa84131
Tran Hoang Nam (PPM) <namth2@bagroup.vn>
2024-04-17 14:21:29 +0700

    .

---
+++ PhotoBrowser.Maui/Platforms/iOS/Services/MyMWPhotoBrower.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Maui.Controls.Compatibility.Platform.iOS;
+using Microsoft.Maui.Platform;
+using MWPhotoBrowsers;
+using PhotoBrowsers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UIKit;
+
+namespace PhotoBrowsers.Platforms.iOS
+{
+    public class MyMWPhotoBrower : MWPhotoBrowserDelegate
+    {
+        protected PhotoBrowser _photoBrowser;
+
+        protected List<MWPhoto> _photos = new List<MWPhoto>();
+
+        public MyMWPhotoBrower(PhotoBrowser photoBrowser)
+        {
+            _photoBrowser = photoBrowser;
+        }
+
+        public void Show()
+        {
+            _photos = new List<MWPhoto>();
+
+            foreach (Photo p in _photoBrowser.Photos)
+            {
+                MWPhoto mp = MWPhoto.FromUrl(new Foundation.NSUrl(p.URL));
+
+                if (!string.IsNullOrWhiteSpace(p.Title))
+                    mp.Caption = p.Title;
+
+                _photos.Add(mp);
+            }
+
+            MWPhotoBrowser browser = new MWPhotoBrowser(this)
+            {
+                EnableGrid = true,
+
+                BrowserBackgroundColor = Colors.Black.ToPlatform(),
+
+                ZoomPhotosToFill = true
+
+            };
+
+
+            browser.SetCurrentPhoto((nuint)0);
+
+
+            var window = UIApplication.SharedApplication.KeyWindow;
+            var vc = window.RootViewController;
+            while (vc.PresentedViewController != null)
+            {
+                vc = vc.PresentedViewController;
+            }
+
+            vc.PresentViewController(new UINavigationController(browser), true, null);
+        }
+
+        public override MWPhoto GetPhoto(MWPhotoBrowser photoBrowser, nuint index) => _photos[(int)index];
+
+        public override nuint NumberOfPhotosInPhotoBrowser(MWPhotoBrowser photoBrowser) => (nuint)_photos.Count;
+
+        public void Close()
+        {
+            UIApplication.SharedApplication.KeyWindow.RootViewController.DismissViewController(true, null);
+        }
+    }
+}
--- PhotoBrowser.Maui/Platforms/iOS/Services/PhotoBrowserImplementation.cs
+++ PhotoBrowser.Maui/Platforms/iOS/Services/PhotoBrowserImplementation.cs
@@ -1,31 +1,24 @@
 ﻿using Foundation;
 using IDMPhotoBrowserBindings;
 using UIKit;
-using static Microsoft.Maui.ApplicationModel.Permissions;
 
 namespace PhotoBrowsers.Platforms.iOS
 {
     public class PhotoBrowserImplementation : IPhotoBrowser
     {
+        protected static MyMWPhotoBrower _mainBrowser;
         public void Show(PhotoBrowser photoBrowser)
         {
-            var photos = photoBrowser.Photos.Select(x => new IDMPhoto(NSUrl.FromString(x.URL)!)).ToArray();
-            var browser = new IDMPhotoBrowser(photos);
-            browser.Title = "";
-            browser.UsePopAnimation = true;
-            browser.UsePopAnimation = true;
-            var window = UIApplication.SharedApplication.KeyWindow;
-            var vc = window.RootViewController;
-            while (vc.PresentedViewController != null)
-            {
-                vc = vc.PresentedViewController;
-            }
-
-            vc.PresentViewController(new UINavigationController(browser), true, null);
+            _mainBrowser = new MyMWPhotoBrower(photoBrowser);
+            _mainBrowser.Show();
         }
         public void Close()
         {
-            UIApplication.SharedApplication.KeyWindow.RootViewController.DismissViewController(true, null);
+            if (_mainBrowser != null)
+            {
+                _mainBrowser.Close();
+                _mainBrowser = null;
+            }
         }
     }
 }
